6975c4546103a397ac86a9624b990807559578f9
Leandro Heidemann da Silva <leandro.heidemann@outlook.com>
2022-11-11 13:59:03 -0300

    first commit

---
+++ src/main/java/net/unibave/avaapi/AvaApiApplication.java
@@ -0,0 +1,13 @@
+package net.unibave.avaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/unibave/avaapi/AvaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.unibave.avaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
